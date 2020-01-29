f6f69b8ef16291d3a00a5e1fb904efcb630a1fe8
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-01-29 18:04:24 +0100

    Create ft_memmove.c

---
+++ libft.a/ft_memmove.c
@@ -0,0 +1,23 @@
+#include <string.h>
+void *ft_memmove(void *dst, const void *src, size_t n)
+{
+  char  *dest;
+  char  *source;
+  int i;
+
+  dest = dst;
+  source = src;
+  i = 0;
+  if (source < dest)
+  {
+    while ()
+  }
+  else
+  {
+    while (len-- > 0)
+    {
+
+    }
+  }
+  return (dest);
+}
