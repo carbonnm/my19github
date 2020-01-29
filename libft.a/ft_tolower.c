161c0bd937f4cbfa2b3337eb4dad74904a8ed3d9
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-01-29 14:06:19 +0100

    ft_isdigit.c

---
+++ libft.a/ft_tolower.c
@@ -0,0 +1,6 @@
+int ft_tolower(int c)
+{
+  if ((n >= ) && (n <= ))
+    return (1);
+  return (0);
+}
---
+++ libft.a/ft_toupper.c
@@ -0,0 +1,6 @@
+int ft_toupper(int c)
+{
+  if ((n >= ) && (n <= ))
+    return (1);
+  return (0);
+}
