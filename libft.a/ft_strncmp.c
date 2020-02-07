dc2bb9c66dba29acb0d71da0edce408cfdb3eb40
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-07 13:05:51 +0100

    Update ft_strncmp.c

--- libft.a/ft_strncmp.c
+++ libft.a/ft_strncmp.c
@@ -6,11 +6,11 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:29:28 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:38:44 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/07 12:56:02 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
   int i;
