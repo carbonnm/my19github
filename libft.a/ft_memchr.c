795df457d5d031cca5f14b65e7bbc86f7531e833
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-07 13:03:52 +0100

    Update ft_memchr.c

--- libft.a/ft_memchr.c
+++ libft.a/ft_memchr.c
@@ -6,11 +6,11 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:28:20 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:38:03 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/07 12:54:24 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 void *ft_memchr(const void *s, int c, size_t n)
 {
   char  *str;
