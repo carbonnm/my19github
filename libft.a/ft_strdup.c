613821a4af196391e3b2096bdde57cb07b4b26f4
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-14 14:52:19 +0100

    Update ft_strdup.c

--- libft.a/ft_strdup.c
+++ libft.a/ft_strdup.c
@@ -10,3 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+char  *ft_strdup(char *src)
+{
+  char  *new;
+  int   i;
+  int   size;
+
+  size = 0;
+  i = 0;
+  while (src[size])
+    src++;
+  if (!(new = malloc(sizeof(char) * (size + 1))))
+    return (NULL);
+  while (src[i])
+  {
+    new[i] = src[i];
+    i++;
+  }
+  new[i] = '\0';
+  return (new);
+}
