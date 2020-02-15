cdcdfb99fd22dd7fd414276548127e90b6f8369d
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 14:58:15 +0100

    Update ft_calloc.c

--- libft.a/ft_calloc.c
+++ libft.a/ft_calloc.c
@@ -6,16 +6,22 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:26:21 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/15 13:26:50 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 14:58:08 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*Allocation et liberation dynamique de la memoire.
-Calloc alloue la mémoire nécessaire pour un tableau de nmemb éléments
+Calloc alloue la mémoire nécessaire pour un tableau de count éléments
 de taille size octets, et renvoie un pointeur vers la mémoire allouée.
 Elle retourne un pointeur sur la mémoire allouée, qui est correctement
 alignée pour n'importe quel type de variable.
 Sinon NULL.
+Si le nombre d'elements dans le tableau et la taille des octets est nulle,
+je retourne NULL.
+Le resultat sera la memoire necessaire a la taille * nb elements.
+Il ne doit pas etre NULL sinon je retourne NULL.
+Toutes les confitions sont remplies : je remplis donc mon tableau
+avec des 0 sur tout le resultat necessaire et je le retourne.
 */
 
 #include "libft.h"
