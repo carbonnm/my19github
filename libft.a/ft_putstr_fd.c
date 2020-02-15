bb26206cc2c2ed07a80fa0d19303e92ff90cb8f3
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 14:34:49 +0100

    Update ft_putstr_fd.c

--- libft.a/ft_putstr_fd.c
+++ libft.a/ft_putstr_fd.c
@@ -6,9 +6,14 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 11:32:55 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/15 11:33:05 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 14:34:45 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Ecrit la chaine de caractere s sur le file descriptor donne.
+Je fais appel a ft_putchar_fd pour pouvoir ecrire ma chaine de
+caracteres caractere par caractere.
+*/
 
 #include "libft.h"
 void ft_putstr(char const *s, int fd)
