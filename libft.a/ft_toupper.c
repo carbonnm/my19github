0ca1d94ea0b189b1b129d492403a6397562e1d59
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:52:38 +0100

    Update ft_toupper.c

--- libft.a/ft_toupper.c
+++ libft.a/ft_toupper.c
@@ -6,9 +6,12 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:31:12 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:57:06 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:52:34 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Verifie si un caractere qui peut etre converti en majuscule.
+*/
 
 #include "libft.h"
 int ft_toupper(int c)
