3eff50d767000fad31930abb211e356aec67a2e8
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 13:17:01 +0100

    Update ft_strlcpy.c

--- libft.a/ft_strlcpy.c
+++ libft.a/ft_strlcpy.c
@@ -6,9 +6,15 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:29:18 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:55:49 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 13:16:56 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Copie de chaines limitees par la taille.
+Elle copie jusqu'a la taille - 1 caracteres de la chaine terminee
+par NULL src vers dst, terminant le resultat par NULL.
+Elle renvoie la longueur totale de src.
+*/
 
 #include "libft.h"
 size_t  ft_strlcpy(char * restrict dst, const char * restrict src, size_t size)
