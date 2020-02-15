72732fc41e761d57d48786f28603e1beb813fd41
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:14:16 +0100

    Update ft_bzero.c

--- libft.a/ft_bzero.c
+++ libft.a/ft_bzero.c
@@ -6,9 +6,15 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:36:40 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 13:09:07 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:13:50 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Bzero ecrit des octets de valeur zero dans un bloc d'octets.
+Elle va mettre a 0 les n premiers octets du bloc memoire pointe par s.
+Je cree donc a un nouveau un pointeur qui sera egal a mon s.
+Je vais venir le remplir avec des '\0'.
+*/
 
 #include "libft.h"
 void	ft_bzero(void *s, size_t, n)
