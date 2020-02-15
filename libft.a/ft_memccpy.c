7ba76151c71038a4c169bc613d5be8362f63cf82
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 15:04:52 +0100

    Update ft_memccpy.c

--- libft.a/ft_memccpy.c
+++ libft.a/ft_memccpy.c
@@ -6,9 +6,18 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:27:56 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:54:11 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 15:04:49 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Cette fonction copie AU PLUS n octets de la zone memoire src vers
+la zone memoire dst en s'arretant des qu'elle rencontre le caractere c.
+Elle renvoie un pointeur sur le caractere immediatement apres c dans dest.
+Sinon NULL.
+Je cree donc les deux pointeurs s'apparentant a dst et src.
+Si je trouve une occurence dans les n premiers octets, je retourne
+un pointeur sur le caractere durect apres.
+*/
 
 #include "libft.h"
 void *ft_memccpy(void *dst, const void *src, int c, size_t n)
