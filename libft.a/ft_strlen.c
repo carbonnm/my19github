fb5e6340a10ec42971c9c8017737d9ce8531e083
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:46:34 +0100

    Update ft_strlen.c

--- libft.a/ft_strlen.c
+++ libft.a/ft_strlen.c
@@ -6,9 +6,13 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:29:23 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:58:22 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:46:30 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Cette fonction calcule la longueur d'une chaine de caracteres.
+Elle s'incrementera jusqu'a '\0'.
+*/
 
 #include "libft.h"
 size_t  ft_strlen(char *s)
