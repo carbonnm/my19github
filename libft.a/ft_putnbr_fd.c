4037428e69a2f649e6c2c95fc87eb228dec27475
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 14:44:15 +0100

    Update ft_putnbr_fd.c

--- libft.a/ft_putnbr_fd.c
+++ libft.a/ft_putnbr_fd.c
@@ -6,9 +6,16 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 11:32:47 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/15 11:32:49 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 14:44:10 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Ecrire l'integer n sur le file descriptor donne.
+J'envisage l'exception avec ft_pustr_fd.
+Si le nombre est negatif, j'ecris le - avec ft_putchar_fd.
+S'il est plus grand que 9, je le divise par 10, de maniere recursive,
+jusqu'a ce qu'il ne soit pas plus grand que 9.
+*/
 
 #include "libft.h"
 void ft_putnbr_fd(int n, int fd)
