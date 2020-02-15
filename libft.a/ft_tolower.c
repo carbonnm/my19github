0d16c60f1f82becadb05a11b4b65a73b1c52c6f6
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:53:27 +0100

    Update ft_tolower.c

--- libft.a/ft_tolower.c
+++ libft.a/ft_tolower.c
@@ -6,9 +6,12 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:31:05 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:57:20 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:53:24 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Verifie si on a un caractere qui peut etre converti en minuscule.
+*/
 
 #include "libft.h"
 int ft_tolower(int c)
