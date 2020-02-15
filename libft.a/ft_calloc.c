c35461e42d00a3e29b41cdadc23d681f0cae207e
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 13:26:54 +0100

    Update ft_calloc.c

--- libft.a/ft_calloc.c
+++ libft.a/ft_calloc.c
@@ -6,9 +6,17 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:26:21 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 13:24:08 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 13:26:50 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Allocation et liberation dynamique de la memoire.
+Calloc alloue la mémoire nécessaire pour un tableau de nmemb éléments
+de taille size octets, et renvoie un pointeur vers la mémoire allouée.
+Elle retourne un pointeur sur la mémoire allouée, qui est correctement
+alignée pour n'importe quel type de variable.
+Sinon NULL.
+*/
 
 #include "libft.h"
 void *ft_calloc(size_t count, size_t size)
