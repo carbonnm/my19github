f43eaf6cd190fb34f97d85b36a96f3dcb4fea5c6
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:41:35 +0100

    Update ft_memchr.c

--- libft.a/ft_memchr.c
+++ libft.a/ft_memchr.c
@@ -6,9 +6,18 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:28:20 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:54:24 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:41:30 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Cette fonction sert a rechercher un caractere dans une zone memoire.
+Elle examine les n premiers octets de la zone memoire pointee par s.
+Des qu'elle rencontre le caractere c, elle arrete de chercher.
+je vais creer a nouveau un pointeur correspondant a s.
+Je parcours la chaine de caractere.
+Si je trouve c, je renvoie un pointeur sur l'octet correspondant.
+Sinon NULL.
+*/
 
 #include "libft.h"
 void *ft_memchr(const void *s, int c, size_t n)
