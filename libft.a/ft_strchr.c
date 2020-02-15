6b28444aa41fd1abdac720282c6a0c0cacf82389
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:55:57 +0100

    Update ft_strchr.c

--- libft.a/ft_strchr.c
+++ libft.a/ft_strchr.c
@@ -6,9 +6,16 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:29:03 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:55:25 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:55:51 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Cette fonction recherche une caractere dans une chaine de caractere.
+Elle renvoie un pointeur sur la premiere occurence du caractere c dans s.
+Je parcours donc juste la chaine.
+Si je trouve l'occurence, je la retourne.
+Sinon NULL.
+*/
 
 #include "libft.h"
 char  *ft_strchr(const char *s, int c)
