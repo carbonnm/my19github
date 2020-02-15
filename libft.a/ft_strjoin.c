93d152d73e78b86fc6144837622e006a02fa2294
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 15:08:16 +0100

    Update ft_strjoin.c

--- libft.a/ft_strjoin.c
+++ libft.a/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 11:33:25 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/15 14:22:40 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 15:08:10 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,13 @@ char  ft_strjoin(char const *s1, char const *s2)
   int   j;
   char  *str;
 
-  i = 0;
   j = 0;
   i = ft_strlen(s1) + ft_strlen(s2) + 1;
   if (!(str = (char *)malloc(sizeof(char) * i)))
     return (NULL);
   if (s1)
     while(s1[j])
-      {
         str[i++] = s1[j++];
-      }
   j = 0;
   if (s2)
     while (s2[j])
