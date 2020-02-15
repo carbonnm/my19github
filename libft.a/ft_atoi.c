f49032738ff960c38c799f2415f37456258ea6ab
carbonnm <55496761+carbonnm@users.noreply.github.com>
2020-02-15 12:01:23 +0100

    Update ft_atoi.c

--- libft.a/ft_atoi.c
+++ libft.a/ft_atoi.c
@@ -6,9 +6,22 @@
 /*   By: cmarie <cmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 12:52:46 by cmarie            #+#    #+#             */
-/*   Updated: 2020/02/07 12:53:22 by cmarie           ###   ########.fr       */
+/*   Updated: 2020/02/15 12:01:14 by cmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*atoi permet de convertir une chaine de caracteres en un entier.
+Cette fonction prend donc en parametre une chaine de caracteres.
+J'initialise le resultat et le fait d'etre negatif ou pas (qui va jouer).
+Je mets par defaut la variable negative a 1, je ;a changerai a -1 si c'est negatif.
+En premier lieu, tant qu'il y a les caracteres speciaux precises au debut,
+j'avance dans la chaine de caracteres car ces caracteres ne sont pas
+a prendre en compte.
+Ensuite, si le pre;ier caractere de signe rencontre est un -, negative = -1.
+Je ne prends par apres plus les caracteres de signe en compte.
+Tant que le caractere est entre 0 et 9, il le convertit.
+Il retourne le resultat * la variable negative pour avoir le nombre correct.
+*/
 
 #include "libft.h"
 int ft_atoi(const char *str)
